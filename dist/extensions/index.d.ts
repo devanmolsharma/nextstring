fd9a171029169c1945f915f2f128b6cc86f30b75
Anmol Sharma <keepupsharma@gmail.com>
2025-05-21 20:52:53 -0500

    feat: add CheckIf and ExtractData to extensions; include Question handling

--- dist/extensions/index.d.ts
+++ dist/extensions/index.d.ts
@@ -1,6 +1,12 @@
+import { CheckIf } from "./checkIf";
+import { ExtractData } from "./extractData";
+import { Question } from "./question";
 import { Summarise } from "./summarise";
 export * from "./extension";
 export declare const extensions: {
     readonly summarise: typeof Summarise.handle;
+    readonly question: typeof Question.handle;
+    readonly checkIf: typeof CheckIf.handle;
+    readonly extractData: typeof ExtractData.handle;
 };
 //# sourceMappingURL=index.d.ts.map
--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,6 +1,12 @@
+import { CheckIf } from "./checkIf";
+import { ExtractData } from "./extractData";
+import { Question } from "./question";
 import { Summarise } from "./summarise";
 export * from "./extension";
 
 export const extensions = {
   [Summarise.functionName]: Summarise.handle,
+  [Question.functionName]: Question.handle,
+  [CheckIf.functionName]: CheckIf.handle,
+  [ExtractData.functionName]: ExtractData.handle,
 } as const;
