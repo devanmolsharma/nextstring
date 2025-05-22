19960bba6faa8d2b86c673530e63d85c86674ed7
Anmol Sharma <keepupsharma@gmail.com>
2025-05-22 18:49:13 -0500

    refactor: restructure index.ts and move main logic to main.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,2 @@
-import { OpenaiProvider } from "./providers";
-import { extensions } from "./extensions";
-import { Provider } from "./providers/provider";
-type ExtentionsType = typeof extensions;
-declare global {
-    interface String extends ExtentionsType {
-    }
-}
-declare function initialise(provider: Provider): void;
-declare const _default: {
-    OpenaiProvider: typeof OpenaiProvider;
-    initialise: typeof initialise;
-};
-export default _default;
+export * from "./extensions";
+export * from "./main";
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,2 @@
-import { OpenaiProvider } from "./providers";
-import { extensions } from "./extensions";
-import { Provider } from "./providers/provider";
-import { ProviderManager } from "./providers/provider.manager";
-
-type ExtentionsType = typeof extensions;
-
-declare global {
-  interface String extends ExtentionsType {}
-}
-
-// Extend the String prototype with the extensions
-for (const key of Object.keys(extensions)) {
-  // @ts-ignore
-  String.prototype[key] = function (...args: any[]) {
-    // @ts-ignore
-    return extensions[key as keyof typeof extensions].apply(this, args);
-  };
-}
-
-function initialise(provider: Provider) {
-  ProviderManager.setProvider(provider);
-}
-
-export default { OpenaiProvider, initialise };
+export * from "./extensions";
+export * from "./main";
